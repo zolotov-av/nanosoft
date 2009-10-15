2ab5619376f0b6974a244b0bd4dcac231a31e530
Alex V. Zolotov <shade@shamangrad.net>
2009-10-15 18:41:06 +0000

    + nanosoft/error.h

---
+++ trunk/src/lib/error.cpp
@@ -0,0 +1,57 @@
+
+#include <errno.h>
+#include <string.h>
+#include <string>
+#include <stdexcept>
+#include <iostream>
+#include <nanosoft/error.h>
+
+using namespace std;
+
+namespace nanosoft
+{
+	/**
+	* Сгенерировать исключение (runtime_error)
+	* @param message сообщение об ошибке
+	* @note сообщение об ошибке сразу же выводиться в stderr
+	*/
+	void error(const char *message)
+	{
+		cerr << message << endl;
+		throw runtime_error(message);
+	}
+	
+	/**
+	* Сгенерировать исключение (runtime_error)
+	* @param message сообщение об ошибке
+	* @note сообщение об ошибке сразу же выводиться в stderr
+	*/
+	void error(const std::string &message)
+	{
+		cerr << message << endl;
+		throw std::runtime_error(message);
+	}
+	
+	/**
+	* Вернуть стандартное сообщение о последней
+	* ошибке вызова стандартной функции C
+	*/
+	const char * stderror()
+	{
+		return strerror(errno);
+	}
+	
+	/**
+	* Проверить статус вызова стандартной функции C
+	* и в случае ошибки сгенерировать исключение
+	* @param status если 0, то сгенерировать исключение
+	* @note сообщение об ошибке сразу же выводиться в stderr
+	*/
+	void stdcheck(int status)
+	{
+		if ( status == 0 )
+		{
+			error(stderror());
+		}
+	}
+}
