6367a24d563d88cd45b1a5547b58e267f34d6746
Alex V. Zolotov <shade@shamangrad.net>
2014-06-14 11:43:16 +0000

    + тестовый проект для avr

---
+++ avr/trunk/main.c
@@ -0,0 +1,76 @@
+
+//#define F_CPU 1000000UL  // 1 MHz
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+
+#include "pictl.h"
+
+#define DD_MISO PB6
+
+volatile char counter = 1;
+
+ISR(SPISTC_vect)
+{
+	pictl_put(SPDR);
+	SPDR = pictl_fetch();
+}
+
+void command1(const struct pictl_packet *packet)
+{
+	if ( packet->len >= 4 )
+	{
+		unsigned char pin = packet->opt[0];
+		if ( pin < 8 )
+		{
+			PORTA |= (1 << pin);
+		}
+	}
+}
+
+void command2(const struct pictl_packet *packet)
+{
+	if ( packet->len >= 4 )
+	{
+		unsigned char pin = packet->opt[0];
+		if ( pin < 8 )
+		{
+			PORTA &= ~(1 << pin);
+		}
+	}
+}
+
+void pictl_on_packet(const struct pictl_packet *packet)
+{
+	switch (packet->cmd)
+	{
+	case 1:
+		command1(packet);
+		return;
+	case 2:
+		command2(packet);
+		return;
+	}
+}
+
+int main()
+{
+	// init SPI slave
+	MISO_DDR = (1<<MISO_BIT);
+	DDRA = 0xFF;
+	SPCR = (1 << SPIE) | (1 << SPE);
+	PORTA = counter;
+	SPDR = counter;
+	pictl_init(pictl_on_packet);
+	while ( 1 )
+	{
+		//PORTA = spi_read(counter++);
+		sei();
+		set_sleep_mode(0);
+		sleep_enable();
+		sleep_cpu();
+	}
+	
+	return 0;
+}
