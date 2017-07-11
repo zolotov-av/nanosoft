480045d2a623e38df19f35a01006066d9a262ff5
Alex V. Zolotov <shade@shamangrad.net>
2017-07-11 08:48:18 +0000

    + заготовка avc_server - сервер видеоконференций

---
+++ ffcairo/avc_server.cpp
@@ -0,0 +1,92 @@
+/****************************************************************************
+
+Сервер видеоконференций
+
+****************************************************************************/
+
+#include <nanosoft/logger.h>
+#include <nanosoft/debug.h>
+#include <nanosoft/netdaemon.h>
+#include <nanosoft/asyncdns.h>
+
+#include <stdio.h>
+
+struct opts_t
+{
+	bool fork;
+};
+
+opts_t opts;
+
+/**
+ * Парсер опций
+ */
+void parse_options(int argc, char** argv)
+{
+	// init default options
+	opts.fork = false;
+	
+	for(int i = 1; i < argc; i++)
+	{
+		if ( strcmp(argv[i], "-f") == 0 )
+		{
+			opts.fork = true;
+			continue;
+		}
+		
+		fprintf(stderr, "unknown option [%s]\n", argv[i]);
+	}
+}
+
+/**
+ * Форкнуть демона
+ */
+void forkDaemon()
+{
+	printf("fork daemon\n");
+	
+	pid_t pid = fork();
+	
+	// Если полученный родительским процессом pid меньше 0, значит, форкнуться не удалось
+	if ( pid < 0 )
+	{
+		logger.error("fork failure: %s", strerror(errno));
+		return;
+	}
+	
+	if ( pid == 0 )
+	{
+		// потомок
+		return;
+	}
+	else
+	{
+		// родитель
+		logger.information("daemon forked, PID: %u", pid);
+		exit(0);
+	}
+}
+
+int main(int argc, char** argv)
+{
+	logger.open("avc_server.log");
+	logger.information("avc_server start");
+	
+	parse_options(argc, argv);
+	
+	if ( opts.fork ) forkDaemon();
+	
+	NetDaemon daemon(100, 1024);
+	
+	adns = new AsyncDNS(&daemon);
+	
+	daemon.addObject(adns);
+	
+	//daemon.setGlobalTimer(onTimer, &daemon);
+	
+	daemon.run();
+	
+	logger.information("avc_server exit");
+	printf("avc_server exit\n");
+	return 0;
+}
