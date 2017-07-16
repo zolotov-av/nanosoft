584029506b9dfd26843843aac645ee6b085fd6e0
Alex V. Zolotov <shade@shamangrad.net>
2017-07-16 13:12:08 +0000

    + класс BlocksPool - пул блоков памяти, основан на коде буфера из NetDaemon

--- libnano2/main.cpp
+++ libnano2/main.cpp
@@ -1,6 +1,46 @@
 #include <stdio.h>
 
+#include <nanosoft/blockspool.h>
+
+int count_blocks(nano_block_t *p)
+{
+	int count = 0;
+	while ( p )
+	{
+		count ++;
+		p = p->next;
+	}
+	return count;
+}
+
 int main()
 {
 	printf("Hello world\n");
+	
+	BlocksPool bp;
+	bp.reserve(16);
+	printf("\n");
+	printf("total = %d\n", bp.getTotalCount());
+	printf("free = %d\n", bp.getFreeCount());
+	
+	nano_block_t *blocks = bp.allocBySize(4000);
+	printf("\n");
+	printf("allocated blocks: %d\n", count_blocks(blocks));
+	printf("total = %d\n", bp.getTotalCount());
+	printf("free = %d\n", bp.getFreeCount());
+	
+	bp.reserve(16);
+	printf("\n");
+	printf("total = %d\n", bp.getTotalCount());
+	printf("free = %d\n", bp.getFreeCount());
+	
+	bp.free(blocks);
+	printf("\n");
+	printf("total = %d\n", bp.getTotalCount());
+	printf("free = %d\n", bp.getFreeCount());
+	
+	bp.clear();
+	printf("\n");
+	printf("total = %d\n", bp.getTotalCount());
+	printf("free = %d\n", bp.getFreeCount());
 }
---
+++ libnano2/nanosoft/blockspool.cpp
@@ -0,0 +1,162 @@
+
+#include <nanosoft/blockspool.h>
+
+#include <string.h>
+#include <stdio.h>
+
+/**
+* Конструктор
+*/
+BlocksPool::BlocksPool()
+{
+	total_count = 0;
+	free_count = 0;
+	stack = NULL;
+	pools = NULL;
+}
+
+/**
+* Деструктор
+*
+* Высвобождает все блоки, в том числе те, что выданы клиентам
+*/
+BlocksPool::~BlocksPool()
+{
+	clear();
+}
+
+static void assert_align(void *p)
+{
+	uintptr_t addr = (uintptr_t)p;
+	if ( addr % FDBUFFER_BLOCK_SIZE )
+	{
+		printf("block not aligned\n");
+		exit(-1);
+	}
+}
+
+/**
+* Зарезервировать блоки
+*
+* Запрашивает у системы новые блоки и добавляет их в пул
+*/
+int BlocksPool::reserve(size_t count)
+{
+	size_t info_blocks = (count * sizeof(nano_block_t) + FDBUFFER_BLOCK_SIZE - 1) / FDBUFFER_BLOCK_SIZE;
+	size_t info_size = info_blocks * FDBUFFER_BLOCK_SIZE;
+	size_t data_size = count * FDBUFFER_BLOCK_SIZE;
+	
+	void *p;
+	int ret = posix_memalign(&p, FDBUFFER_BLOCK_SIZE, info_size + data_size);
+	if ( ret != 0 ) return -1;
+	
+	pool_t *pool = new pool_t;
+	pool->data = p;
+	pool->next = pools;
+	pools = pool;
+	
+	nano_block_t *info = (nano_block_t*)p;
+	uint8_t *data = (uint8_t*)p + info_size;
+	
+	for(size_t i = 0; i < count; i++)
+	{
+		info->data = data;
+		info->next = stack;
+		assert_align(info->data);
+		memset(info->data, 0, FDBUFFER_BLOCK_SIZE);
+		stack = info;
+		info++;
+		data += FDBUFFER_BLOCK_SIZE;
+	}
+	
+	total_count += count;
+	free_count += count;
+	
+	return -1;
+}
+
+/**
+* Очистить пул
+*
+* Высвобождает все блоки, в том числе те, что выданы клиентам
+*/
+void BlocksPool::clear()
+{
+	while ( pools )
+	{
+		pool_t *pool = pools;
+		pools = pools->next;
+		::free(pool->data);
+		delete pool;
+	}
+	
+	total_count = 0;
+	free_count = 0;
+	stack = 0;
+}
+
+/**
+* Выделить цепочку блоков
+*
+* @param size требуемый размер в байтах
+* @return список блоков или NULL если невозможно выделить запрощенный размер
+*/
+nano_block_t* BlocksPool::allocBySize(size_t size)
+{
+	// размер в блоках
+	size_t count = (size + FDBUFFER_BLOCK_SIZE - 1) / FDBUFFER_BLOCK_SIZE;
+	
+	return allocByBlocks(count);
+}
+
+/**
+* Выделить цепочку блоков
+*
+* @param count требуемый размер в блоках
+* @return список блоков или NULL если невозможно выделить запрощенный размер
+*/
+nano_block_t* BlocksPool::allocByBlocks(size_t count)
+{
+	if ( count == 0 ) return 0;
+	if ( count > free_count ) return 0;
+	
+	nano_block_t *block = stack;
+	if ( count > 1 )
+	{
+		size_t lim = count - 1;
+		for(size_t i = 0; i < lim; i++)
+		{
+			stack = stack->next;
+		}
+	}
+	nano_block_t *last = stack;
+	stack = last->next;
+	last->next = 0;
+	free_count -= count;
+	
+	return block;
+}
+
+/**
+* Освободить цепочку блоков
+*
+* NOTE Этот метод не проверяет принадлежат ли ему эти блоки или другому
+* пулу, просто добавит их в свой пул. Надо быть аккуратным чтобы
+* возвращать блоки в тот пул из которого они были взяты
+*
+* @param list цепочка блоков
+*/
+void BlocksPool::free(nano_block_t *list)
+{
+	if ( list == 0 ) return;
+	nano_block_t *last = list;
+	size_t count = 1;
+	while ( last->next )
+	{
+		count++;
+		last = last->next;
+	}
+	last->next = stack;
+	stack = list;
+	free_count += count;
+}
